4a3da931bed99688362eb86e922656c8142b7407
Samuel Almanzar <samalmanzar@Samuels-MacBook-Air.local>
2018-09-06 12:33:56 -0400

    buttons not producing gifs

--- giphy-homeowrk/gify.js
+++ giphy-homeowrk/gify.js
@@ -2,48 +2,34 @@
   
     // Adding click event listen listener to all buttons
     var hero = $(this).attr("data-hero");
-    var heros = ["superman", "spiderman"];
+    var heros = [];
     $("button").on("click", function() {
-        // Grabbing and storing the data-hero property value from the button
+        
         var hero = $(this).attr("data-hero");
-        // Constructing a queryURL using the hero name
+        
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             hero + "&api_key=e8AZyV4u7AjV4zSczu8miFSXF0R1k9GO&limit=10";
         console.log("hero " + hero);
 
-        // Performing an AJAX request with the queryURL
+        
         $.ajax({
                 url: queryURL,
                 method: "GET"
             })
-            // After data comes back from the request
+            
             .then(function(response) {
                 console.log(queryURL);
-
-
                 console.log(response);
-                // storing the data from the AJAX request in the results variable
                 var results = response.data;
 
-                // Looping through each result item
                 for (var i = 0; i < results.length; i++) {
-
-                    // Creating and storing a div tag
                     var heroDiv = $("<div>");
-
-                    // Creating a paragraph tag with the result item's rating
                     var p = $("<p>").text("Rating: " + results[i].rating);
-
-                    // Creating and storing an image tag
                     var heroImage = $("<img>");
-                    // Setting the src attribute of the image to a property pulled off the result item
                     heroImage.attr("src", results[i].images.fixed_height.url);
 
-                    // Appending the paragraph and image tag to the heroDiv
                     heroDiv.append(p);
                     heroDiv.append(heroImage);
-
-                    // Prependng the heroDiv to the HTML page in the "#gifs-appear-here" div
                     $("#gifs-appear-here").prepend(heroDiv);
                 }
             });
@@ -51,23 +37,15 @@
 
     function renderButtons() {
 
-        // Deleting the heros prior to adding new heros
-        // (this is necessary otherwise you will have repeat buttons)
+
         $("#buttons-view").empty();
 
-        // Looping through the array of heros
         for (var i = 0; i < heros.length; i++) {
 
-            // Then dynamicaly generating buttons for each hero in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var a = $("<button>");
-            // Adding a class of hero-btn to our button
             a.addClass("hero-btn");
-            // Adding a data-attribute
-            a.attr("data-name", heros[i]);
-            // Providing the initial button text
+            a.attr("data-hero", heros[i]);
             a.text(heros[i]);
-            // Adding the button to the buttons-view div
             $("#buttons-view").append(a);
         }
     }
@@ -92,6 +70,7 @@
     // $(document).on("click", ".hero-btn");
     $(".hero-btn").bind("click", function(event) {
         $("<button></button>")
+        var hero = $(this).attr("data-hero");
     })
 
 
